2295c5c11af37bbb7ec08fe9a76accc278bb3fe2
Talles L <talleslasmar@gmail.com>
2015-07-13 15:00:11 -0300

    existing code

---
+++ NotDefault/NotDefaultExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace NotDefault
+{
+    using System.Collections.Generic;
+
+    public static class NotDefaultExtension
+    {
+        /// <summary>
+        /// Returns true if T is default(T), false otherwise.
+        /// </summary>
+        /// <typeparam name="T">Type to check</typeparam>
+        /// <param name="o">Object to check</param>
+        /// <returns>If T is default(T)</returns>
+        public static bool NotDefault<T>(this T o)
+        {
+            return !EqualityComparer<T>.Default.Equals(o, default(T));
+        }
+    }
+}
