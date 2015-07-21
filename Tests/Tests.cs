fbc12b12f1697582239ee02e2e0e82338e3e0c31
Talles L <talleslasmar@gmail.com>
2015-07-21 12:33:56 -0300

    no need for casting

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -9,155 +9,171 @@
         [TestMethod]
         public void Byte()
         {
-            var @byte = (byte)123;
-            var @sbyte = (sbyte)123;
+            byte @byte = 123;
+            byte? nbyte = 123;
 
             // byte
             Assert.IsTrue(@byte.NotDefault());
             Assert.IsFalse(default(byte).NotDefault());
 
+            // byte?
+            Assert.IsTrue(nbyte.NotDefault());
+            Assert.IsFalse(default(byte?).NotDefault());
+
+            sbyte @sbyte = 123;
+            sbyte? nsbyte = 123;
+
             // sbyte
             Assert.IsTrue(@sbyte.NotDefault());
             Assert.IsFalse(default(sbyte).NotDefault());
 
-            // byte?
-            Assert.IsTrue((@byte as byte?).NotDefault());
-            Assert.IsFalse(default(byte?).NotDefault());
-
             // sbyte?
-            Assert.IsTrue((@sbyte as sbyte?).NotDefault());
+            Assert.IsTrue(nsbyte.NotDefault());
             Assert.IsFalse(default(sbyte?).NotDefault());
         }
 
         [TestMethod]
         public void Short()
         {
-            var @short = (short)123;
-            var @ushort = (ushort)123;
+            short @short = 123;
+            short? nshort = 123;
 
             // short
             Assert.IsTrue(@short.NotDefault());
             Assert.IsFalse(default(short).NotDefault());
 
+            // short?
+            Assert.IsTrue(nshort.NotDefault());
+            Assert.IsFalse(default(short?).NotDefault());
+
+            ushort @ushort = 123;
+            ushort? nushort = 123;
+
             // ushort
             Assert.IsTrue(@ushort.NotDefault());
             Assert.IsFalse(default(ushort).NotDefault());
 
-            // short?
-            Assert.IsTrue((@short as short?).NotDefault());
-            Assert.IsFalse(default(short?).NotDefault());
-
             // ushort?
-            Assert.IsTrue((@ushort as ushort?).NotDefault());
+            Assert.IsTrue(nushort.NotDefault());
             Assert.IsFalse(default(ushort?).NotDefault());
         }
 
         [TestMethod]
         public void Int()
         {
-            var @int = 123;
-            var @uint = (uint)123;
+            int @int = 123;
+            int? nint = 123;
 
             // int
             Assert.IsTrue(@int.NotDefault());
             Assert.IsFalse(default(int).NotDefault());
 
+            // int?
+            Assert.IsTrue(nint.NotDefault());
+            Assert.IsFalse(default(int?).NotDefault());
+
+            uint @uint = 123u;
+            uint? nuint = 123u;
+
             // uint
             Assert.IsTrue(@uint.NotDefault());
             Assert.IsFalse(default(uint).NotDefault());
 
-            // int?
-            Assert.IsTrue((@int as int?).NotDefault());
-            Assert.IsFalse(default(int?).NotDefault());
-
             // uint?
-            Assert.IsTrue((@uint as uint?).NotDefault());
+            Assert.IsTrue(nuint.NotDefault());
             Assert.IsFalse(default(uint?).NotDefault());
         }
 
         [TestMethod]
         public void Long()
         {
-            var @long = (long)123;
-            var @ulong = (ulong)123;
+            long @long = 123L;
+            long? nlong = 123L;
 
             // long
             Assert.IsTrue(@long.NotDefault());
             Assert.IsFalse(default(long).NotDefault());
 
+            // long?
+            Assert.IsTrue(nlong.NotDefault());
+            Assert.IsFalse(default(long?).NotDefault());
+
+            ulong @ulong = 123ul;
+            ulong? nulong = 123ul;
+
             // ulong
             Assert.IsTrue(@ulong.NotDefault());
             Assert.IsFalse(default(ulong).NotDefault());
 
-            // long?
-            Assert.IsTrue((@long as long?).NotDefault());
-            Assert.IsFalse(default(long?).NotDefault());
-
             // ulong?
-            Assert.IsTrue((@ulong as ulong?).NotDefault());
+            Assert.IsTrue(nulong.NotDefault());
             Assert.IsFalse(default(ulong?).NotDefault());
         }
 
         [TestMethod]
         public void Float()
         {
-            var @float = (float)12.3;
+            float @float = 12.3f;
+            float? nfloat = 12.3f; 
 
             // float
             Assert.IsTrue(@float.NotDefault());
             Assert.IsFalse(default(float).NotDefault());
 
             // float?
-            Assert.IsTrue((@float as float?).NotDefault());
+            Assert.IsTrue(nfloat.NotDefault());
             Assert.IsFalse(default(float?).NotDefault());
         }
 
         [TestMethod]
         public void Double()
         {
-            var @double = 12.3;
+            double @double = 12.3d;
+            double? ndouble = 12.3d;
 
             // double
             Assert.IsTrue(@double.NotDefault());
             Assert.IsFalse(default(double).NotDefault());
 
             // double
-            Assert.IsTrue((@double as double?).NotDefault());
+            Assert.IsTrue(ndouble.NotDefault());
             Assert.IsFalse(default(double?).NotDefault());
         }
 
         [TestMethod]
         public void Decimal()
         {
-            var @decimal = (decimal)12.3;
+            decimal @decimal = 12.3m;
+            decimal? ndecimal = 12.3m;
 
             // decimal
             Assert.IsTrue(@decimal.NotDefault());
             Assert.IsFalse(default(decimal).NotDefault());
 
             // decimal?
-            Assert.IsTrue((@decimal as decimal?).NotDefault());
+            Assert.IsTrue(ndecimal.NotDefault());
             Assert.IsFalse(default(decimal?).NotDefault());
         }
 
         [TestMethod]
         public void Char()
         {
-            var @char = 's';
+            char @char = 's';
+            char? nchar = 's';
 
             // char
             Assert.IsTrue(@char.NotDefault());
             Assert.IsFalse(default(char).NotDefault());
 
             // char?
-            Assert.IsTrue((@char as char?).NotDefault());
+            Assert.IsTrue(nchar.NotDefault());
             Assert.IsFalse(default(char?).NotDefault());
         }
 
         [TestMethod]
         public void String()
         {
-            var @string = "string";
+            string @string = "string";
 
             Assert.IsTrue(@string.NotDefault());
             Assert.IsFalse(default(string).NotDefault());
@@ -166,16 +182,20 @@
         [TestMethod]
         public void Bool()
         {
-            var @bool = true;
+            bool @bool = true;
+            bool? nbool = true;
 
             Assert.IsTrue(@bool.NotDefault());
             Assert.IsFalse(default(bool).NotDefault());
+
+            Assert.IsTrue(nbool.NotDefault());
+            Assert.IsFalse(default(bool?).NotDefault());
         }
 
         [TestMethod]
         public void Object()
         {
-            var @object = (object)new { Foo = "Bar" };
+            object @object = new { Foo = "Bar" };
 
             Assert.IsTrue(@object.NotDefault());
             Assert.IsFalse(default(object).NotDefault());
