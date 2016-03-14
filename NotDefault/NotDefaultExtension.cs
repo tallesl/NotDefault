3f1a49e3718ebc149ab2323ff49e15b172708543
Talles L <talleslasmar@gmail.com>
2016-03-14 12:32:52 -0300

    enabled code analysis

--- NotDefault/NotDefaultExtension.cs
+++ NotDefault/NotDefaultExtension.cs
@@ -8,11 +8,11 @@
         /// Returns true if T is default(T), false otherwise.
         /// </summary>
         /// <typeparam name="T">Type to check</typeparam>
-        /// <param name="o">Object to check</param>
+        /// <param name="obj">Object to check</param>
         /// <returns>If T is default(T)</returns>
-        public static bool NotDefault<T>(this T o)
+        public static bool NotDefault<T>(this T obj)
         {
-            return !EqualityComparer<T>.Default.Equals(o, default(T));
+            return !EqualityComparer<T>.Default.Equals(obj, default(T));
         }
     }
 }
